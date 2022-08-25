fb5dac1a60b881d28bd14637ae5c8f50433ea130
Timber <tianbo152@gmail.com>
2022-08-26 00:24:31 +0100

    syntex

--- features/annotations/function.ts
+++ features/annotations/function.ts
@@ -8,5 +8,40 @@ const add2 = (a: number, b: number) => {
 };//works too,  but we prefer above one
 
 // const subtract = (a: number, b:number): number => {
-//   a - b;
+//   a - b;// must have a return
 // };
+
+function divide(a: number, b:number): number {
+  return a / b;
+};
+
+const multiply = function(a:number, b:number): number {
+  return a * b;
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+  //return null or return undefined can work too
+
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+const throwError2 = (message: string): string => {
+    if(! message) {
+      throw new Error(message);
+    }
+    return message;
+};
+
+const todayWeather = {
+  date: new Date(),
+  weather: 'sunny'
+};
+const logWeather = ({date, weather}: { date: Date, weather: string}): void => {
+  console.log(date);
+  console.log(weather);
+};
+logWeather(todayWeather);
