f53be54d7d091d21903799e79a533f1238533f08
Timber <tianbo152@gmail.com>
2022-08-25 17:17:58 +0100

    little bit changes

---
+++ features/annotations/variables.ts
@@ -0,0 +1,19 @@
+// const apples: number = 5;
+
+// let bobo: number = 2;
+
+// apples = 2;//error
+
+// bobo = 4;
+
+// bobo = "asdasd";//error
+
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+//built in objects
+
+let now: Date = new Date();
