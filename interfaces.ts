136f2c020948a51a8fc2742fe36a100a1c78c498
Timber <tianbo152@gmail.com>
2022-08-26 17:15:13 +0100

    interface

--- interfaces.ts
+++ interfaces.ts
@@ -11,6 +11,10 @@ interface Vehicle {
    summary(): string;//前面是函数名， 后面是函数返回类型，函数参数也必须一致
 }
 
+interface Vehicle2 {
+  summary(): string;
+}
+
 const oldCivic = {
   name: 'civic',
   year: 2000,
@@ -33,6 +37,17 @@ const wrongTwo = {
   bro: true
 };
 
+
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  //他也有一个summary 方法
+  summary(): string {
+    return `My drink has ${this.sugar} orams of sugar`;
+  }
+};
+
 //太长了，真的不好使，每次都要重复打这些东西， 所以需要interface 增加代码复用性
 const printVehicle = (vehicle: { name: string; year: number; broken:boolean }): void => {
    console.log(`Name: ${vehicle.name}`);
@@ -48,11 +63,20 @@ const printVehicle2 = (vehicle: Vehicle): void => {
   console.log(vehicle.summary());
 };
 
+//只要传入的东西有interface中的东西就行
+const printVehicle3= (vehicle: Vehicle2): void => {
+  //只用了一个也行
+  console.log(vehicle.summary());
+};
+
 printVehicle(oldCivic);
 printVehicle2(oldCivic);
-
+printVehicle3(oldCivic);
 // printVehicle(wrongOne);
 // printVehicle2(wrongOne);这两个就是错的， 因为类型不符合
 
 // printVehicle(wrongTwo);
 // printVehicle2(wrongTwo);这俩也是错的。因为把broken 改成了 bro
+
+
+printVehicle3(drink);//因为 drink 也有 summary ， 所以也能用这个函数， 这里面就抽象的很灵性。
