5adc288cc342576d52657da25f96cc0907547c4f
Timber <tianbo152@gmail.com>
2022-09-01 14:52:20 +0100

    changes

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,10 @@
-console.log('hello')
+/// <reference types="@types/google.maps" />
+import { User } from './User';
+import { Company } from './Company';
+const user = new User();
+console.log(user);
+
+const company = new Company();
+console.log(company);
+//AIzaSyBNLrJhOMz6idD05pzfn5lhA-TAw-mAZCU
+
