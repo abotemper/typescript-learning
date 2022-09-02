f958729273f6ec774e9469ff653ef5f8afab35ed
Timber <tianbo152@gmail.com>
2022-09-02 18:27:32 +0100

    changes

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,17 +1,11 @@
 /// <reference types="@types/google.maps" />
 import { User } from './User';
 import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 const user = new User();
 console.log(user);
 
 const company = new Company();
 console.log(company);
-//AIzaSyBNLrJhOMz6idD05pzfn5lhA-TAw-mAZCU
 
-const map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0
-  }
-});
+const customMap = new CustomMap('map');
