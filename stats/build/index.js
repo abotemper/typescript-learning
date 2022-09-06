3ae7d895a70a69098623ea061c1168656478f7a7
Timber <tianbo152@gmail.com>
2022-09-06 22:10:45 +0100

    tuple

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,2 +1,25 @@
 "use strict";
-console.log('hi');
+Object.defineProperty(exports, "__esModule", { value: true });
+const CsvFileReader_1 = require("./CsvFileReader");
+const MatcheResult_1 = require("./MatcheResult");
+const reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
+const dateOfFirstMatch = reader.data[0][0];
+console.log(dateOfFirstMatch);
+// enum = enumeration
+//给别的程序员看，这里面的东西联系紧密，很敏感， 很有逻辑性，不能乱删
+//一般有一些约定俗成的东西，比如赤橙黄绿青蓝紫，白天黑天，这些可以枚举的东西。
+//然后内容比较少的，比如1860到现在都有哪些年，可以枚举但是太多了。
+// const homeWin = 'H';
+// const awayWin = 'A';
+// const draw = 'D';
+let manUnitedWins = 0;
+for (let match of reader.data) {
+    if (match[1] === 'Man United' && match[5] === MatcheResult_1.MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatcheResult_1.MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games`);
