72319bd75f6aa654a2b95a427dac10f58a49fa72
Timber <tianbo152@gmail.com>
2022-09-09 23:15:50 +0100

    generics

--- features/generics.ts
+++ features/generics.ts
@@ -49,3 +49,34 @@ function printAnything<T>(arr: T[]): void {
 
 printAnything<string>(['a', 'b', 'c']);
 
+// generic constraints
+
+class Car {
+  print() {
+    console.group('im a car');
+  }
+}
+
+class House {
+  print() {
+    console.log('im a house');
+  }
+}
+
+ interface Printable {
+  print(): void;
+ }
+
+ //这个接口进一步限制了T的范围
+function printHouseOrCars<T extends Printable>(arr: T[]):void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+//报错是因为这个数组里没有print这个方法，也就是不在interface范围之内
+printHouseOrCars([1, 2, 3, 4]);
+
+//因为house里有print， 所以可以
+printHouseOrCars<House>([new House(), new House()]);
+printHouseOrCars<Car>([new Car(), new Car()]);
