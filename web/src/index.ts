f6c61b5e37fc484b5b51287c624a7bb61d0a0823
Timber <tianbo152@gmail.com>
2022-09-09 23:55:28 +0100

    last push today

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,8 @@
-console.log('hi');
+import { User } from './models/User';
+
+const user = new User({ name: 'myname', age: 20});
+
+user.set({ name: 'newname', age: 90});
+
+console.log(user.get('name'));
+console.log(user.get('age'));
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,6 +1,7 @@
 interface UserProps {
-  name: string;
-  age: number;
+  //这样的意思是可能有也可能没有，没有没啥关系，这样可以改一个东西
+  name?: string;
+  age?: number;
 }
 
 export class User {
@@ -8,5 +9,9 @@ export class User {
 
   get(propName: string): (number | string) {
     return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
   }
 }
