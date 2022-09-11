055900d12b97f806e74a86a6f34711401d9d9a1a
Timber <tianbo152@gmail.com>
2022-09-11 23:28:25 +0100

    json

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,13 +1,27 @@
-import { User } from './models/User';
+import axios from 'axios';
 
-const user = new User({ name: 'myname', age: 20});
-
-// user.set({ name: 'newname', age: 90});
-
-// console.log(user.get('name'));
-// console.log(user.get('age'));
-user.on('change', () => {});
-user.on('change', () => {});
-user.on('change', () => {
-    console.log(user);
+axios.post('http://localhost:3000/users', {
+    name: 'muname',
+    age: 20
 });
+
+
+// import { User } from './models/User';
+
+// const user = new User({ name: 'myname', age: 20});
+
+// // user.set({ name: 'newname', age: 90});
+
+// // console.log(user.get('name'));
+// // console.log(user.get('age'));
+// user.on('change', () => {
+//     console.log('change 1')
+// });
+// user.on('change', () => {
+//     console.log('change2')
+// });
+// user.on('save', () => {
+//     console.log('save was triggered')
+// });
+
+// user.trigger('change');
