c38614db4db6e9060a99527815a35def0c61fa6d
Timber <tianbo152@gmail.com>
2022-09-14 21:09:09 +0100

    events

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -7,12 +7,9 @@ interface UserProps {
   age?: number;
 }
 
-//这是一个类型， 是一个没有返回值的函数
-type Callback = () => void;
+
 
 export class User {
-  events: { [key: string]: Callback[] } = {};
-
   constructor(private data: UserProps) {}
 
   get(propName: string): (number | string) {
@@ -23,23 +20,7 @@ export class User {
     Object.assign(this.data, update);
   }
 
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers; 
-  }
 
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-
-    if(!handlers || handlers.length === 0) {
-      return;
-    }
-
-    handlers.forEach(callback => {
-      callback();
-    })
-  }
 
   
   fetch(): void {
